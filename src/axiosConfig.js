fe11819ea74ef5d6326863280b2ced426e04e264
Пользователь <krrpacho@gmail.com>
2024-07-29 17:14:28 +0300

    Update

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://prodappback.onrender.com';
+
+
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
