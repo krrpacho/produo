a5d475fb1ae1ec048f9c7419215052d3ae6b99eb
Пользователь <krrpacho@gmail.com>
2024-08-06 17:39:06 +0300

    Update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight, faPen, faClock, faCalendar, faChartBar } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';
 
 const Navbar = ({ onScrollToSection, toggleNavbar }) => {
   const [isExpanded, setIsExpanded] = useState(true);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 540);
 
   const handleToggleNavbar = () => {
     setIsExpanded(!isExpanded);
     toggleNavbar();
   };
 
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 540);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className={`navbar ${isExpanded ? 'expanded' : 'collapsed'}`}>
+    <div className={`navbar ${isExpanded ? 'expanded' : 'collapsed'} ${isMobile ? 'top' : 'left'}`}>
       <div className="navbar-toggle" onClick={handleToggleNavbar}>
         <FontAwesomeIcon icon={isExpanded ? faChevronLeft : faChevronRight} />
       </div>
